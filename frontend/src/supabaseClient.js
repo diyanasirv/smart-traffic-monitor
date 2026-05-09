119e97fdd02ca5373e941d8c3b8e39f41c27d7da
diyanasirv <diyanasir01@gmail.com>
2026-05-10 04:13:01 +0530

    s

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -2,8 +2,7 @@
 // src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://jwgwnblusqjcfmktlngv.supabase.co'; // ✅ REPLACE THIS
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3Z3duYmx1c3FqY2Zta3Rsbmd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0NTQyMzAsImV4cCI6MjA2OTAzMDIzMH0.2N-mBPQa_NMAynw1mCWYm6ul_tvJ7opzmM0UTQ_9Mx0'; // ✅ REPLACE THIS
-
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL; 
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
